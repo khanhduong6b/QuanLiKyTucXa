38e00f7da119cd135e6d227dcd9276bb2238586f
khanhduong6b <khanhduong6b@gmail.com>
2023-12-14 15:21:28 +0700

    Add: xem hoá đơn của sinh viên sử dụng ajax

---
+++ Areas/Admin/Controllers/AnalysisController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLiKyTucXa.Areas.Admin.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        // GET: AnalysisController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(120);
